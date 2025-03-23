b8d806c4b0c2b56529467e13ca03882ace222d12
KenFront <flamma0917@gmail.com>
2025-03-24 03:50:36 +0800

    Add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+// import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id, { getModuleInfo }) {
+          if (id.includes('node_modules')) {
+            const moduleInfo = getModuleInfo(id)
+            const importers = moduleInfo?.importers || []
+            if (importers.length > 1) {
+              return 'vendor'
+            }
+          }
+        }
+      },
+      plugins: [
+        // visualizer({
+        //   filename: 'stats.html',
+        //   open: true
+        // })
+      ]
+    }
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
