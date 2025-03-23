913d15a02954500c6ae30a3f789cc0ee777fb7a5
KenFront <flamma0917@gmail.com>
2025-03-23 20:35:23 +0800

    Make an animation for Ability

--- src/pages/Profile/components/Ability.tsx
+++ src/pages/Profile/components/Ability.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'motion/react'
+import { useEffect, useState } from 'react'
 
 const LABEL_TEXTS = ['CSS', 'React', 'WebGL', 'Git', 'TypeScript', 'Animation']
 
@@ -10,7 +11,7 @@ const generateHexagon = (size: number, offsets: number[] = []) => {
 
   for (let i = 0; i < 6; i++) {
     const angle = (Math.PI / 3) * i
-    const offset = offsets[i] || 1
+    const offset = offsets[i] ?? 1
     const x = centerX + size * Math.cos(angle) * offset
     const y = centerY + size * Math.sin(angle) * offset
     points.push(`${x},${y}`)
@@ -24,11 +25,15 @@ const Ability = () => {
   const hexagonSizes = [35, 28, 21, 14, 7]
   const colors = ['transparent', '#222', '#444', '#666', '#888']
 
-  const vertexScales = [0.75, 0.95, 0.6, 0.8, 0.85, 0.75]
+  const [vertexScales, setVertexScales] = useState([0, 0, 0, 0, 0, 0])
 
   const { labels } = generateHexagon(hexagonSizes[0])
 
   const { hexPoints: mutableHexagon } = generateHexagon(25, vertexScales)
+
+  useEffect(() => {
+    setVertexScales([0.75, 0.95, 0.6, 0.8, 0.85, 0.75])
+  }, [])
 
   return (
     <div className="flex max-h-[50vh] flex-col items-center justify-center bg-gray-200">
@@ -53,7 +58,7 @@ const Ability = () => {
           stroke="blue"
           strokeWidth="2"
           animate={{ points: mutableHexagon }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
+          transition={{ duration: 2 }}
         />
 
         {labels.map(({ x, y, label }, index) => (
